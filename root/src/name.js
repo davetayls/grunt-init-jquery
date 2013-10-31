46c560d27ddad36e15cff04fdb02d7edd953ef1f
davetayls <dave@the-taylors.org>
2013-10-31 09:54:38 +0000

    change name to js_safe_name

--- root/src/name.js
+++ root/src/name.js
@@ -1,5 +1,5 @@
 /*
- * {%= name %}
+ * {%= js_safe_name %}
  * {%= homepage %}
  *
  * Copyright (c) {%= grunt.template.today('yyyy') %} {%= author_name %}
@@ -9,7 +9,7 @@
 (function($) {
 
   // Collection method.
-  $.fn.{%= name %} = function() {
+  $.fn.{%= js_safe_name %} = function() {
     return this.each(function(i) {
       // Do something awesome to each selected element.
       $(this).html('awesome' + i);
@@ -17,20 +17,20 @@
   };
 
   // Static method.
-  $.{%= name %} = function(options) {
+  $.{%= js_safe_name %} = function(options) {
     // Override default options with passed-in options.
-    options = $.extend({}, $.{%= name %}.options, options);
+    options = $.extend({}, $.{%= js_safe_name %}.options, options);
     // Return something awesome.
     return 'awesome' + options.punctuation;
   };
 
   // Static method default options.
-  $.{%= name %}.options = {
+  $.{%= js_safe_name %}.options = {
     punctuation: '.'
   };
 
   // Custom selector.
-  $.expr[':'].{%= name %} = function(elem) {
+  $.expr[':'].{%= js_safe_name %} = function(elem) {
     // Is this element awesome?
     return $(elem).text().indexOf('awesome') !== -1;
   };
